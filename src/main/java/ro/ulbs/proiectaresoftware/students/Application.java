f3d1240f451be14b8363f463a39b8318f2414013
GabrielCristian420 <xxgabygamerxxg@gmail.com>
2026-03-10 09:09:21 +0200

    Ex 2.5.2

--- src/main/java/ro/ulbs/proiectaresoftware/students/Application.java
+++ src/main/java/ro/ulbs/proiectaresoftware/students/Application.java
@@ -1,5 +1,8 @@
 package ro.ulbs.proiectaresoftware.students;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(){
         Student s1 = new Student(112, "Ioan", "Popa", "TI21/1");
@@ -7,11 +10,39 @@ public class Application {
         Student s3 = new Student(120, "Alis", "Popa", "TI21/2");
         Student s4 = new Student(122, "Mihai", "Vecerdea", "TI22/1");
         Student s5 = new Student(122, "Eugen", "Uritescu", "TI22/2");
+        List<Student> studenti = new ArrayList<>();
+        studenti.add(s1);
+        studenti.add(s2);
+        studenti.add(s3);
+        studenti.add(s4);
+        studenti.add(s5);
         System.out.println(String.format("%14s %20s %16s", "numar matricol", "prenume nume", "formatieDeStudiu"));
-        System.out.println(s1);
-        System.out.println(s2);
-        System.out.println(s3);
-        System.out.println(s4);
-        System.out.println(s5);
+        for(Student s : studenti){
+            System.out.println(s);
+        }
+        Student cautat=new Student(120, "Alis", "Popa", "TI21/2");
+        Student cautat2=new Student(112, "Maria", "Popa", "TI21/1");
+        if(existaStudent(studenti,cautat)){
+            System.out.println("Studentul Alis Popa TI21/2 exista");
+        }
+        else{
+            System.out.println("Studentul Alis Popa TI21/2 nu exista");
+        }
+        if(existaStudent(studenti,cautat2)){
+            System.out.println("Studentul Maria Popa TI21/1 exista");
+        }
+        else{
+            System.out.println("Studentul Maria Popa TI21/1 nu exista");
+        }
+    }
+    public static boolean existaStudent(List<Student> lista, Student cautat) {
+        for (Student s : lista) {
+            if (s.getPrenume().equals(cautat.getPrenume()) &&
+                    s.getNume().equals(cautat.getNume()) &&
+                    s.getFormatieDeStudiu().equals(cautat.getFormatieDeStudiu())) {
+                return true;
+            }
+        }
+        return false;
     }
 }
